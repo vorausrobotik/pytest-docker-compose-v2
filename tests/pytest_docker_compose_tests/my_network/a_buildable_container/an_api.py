ee7c8b36dd230008403aafc3a05db636d13744ac
Radu Suciu <radusuciu@gmail.com>
2024-02-27 21:11:46 +0000

    update test application
    was getting some errors regarding the scope of the CONNECTION variable so changed that around

--- tests/pytest_docker_compose_tests/my_network/a_buildable_container/an_api.py
+++ tests/pytest_docker_compose_tests/my_network/a_buildable_container/an_api.py
@@ -1,17 +1,41 @@
+from contextlib import asynccontextmanager, contextmanager
+
 import psycopg2
+import psycopg2.pool as pool
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from psycopg2.extras import RealDictCursor
 
-app = FastAPI()
+db_pool = pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    dbname="postgres",
+    user="postgres",
+    host="my_db",
+    port=5432,
+)
+
+
+def get_db_connection():
+    return db_pool.getconn()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    db_pool.closeall()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 def create_database():
-    with CONNECTION.cursor() as cursor:
+    connection = get_db_connection()
+    with connection.cursor() as cursor:
         cursor.execute(
             "CREATE TABLE IF NOT EXISTS my_table (id serial PRIMARY KEY, num integer, data varchar);"
         )
-        CONNECTION.commit()
+        connection.commit()
 
 
 @app.get("/")
@@ -20,44 +44,48 @@ def read_root():
 
 
 @app.get("/items/all")
-def read_all():
-    with CONNECTION.cursor(cursor_factory=RealDictCursor) as cursor:
+def read_all(connection: psycopg2.extensions.connection = Depends(get_db_connection)):
+    with connection.cursor(cursor_factory=RealDictCursor) as cursor:
         cursor.execute("SELECT * FROM my_table;")
         return [row for row in cursor.fetchall()]
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int):
-    with CONNECTION.cursor(cursor_factory=RealDictCursor) as cursor:
+def read_item(
+    item_id: int,
+    connection: psycopg2.extensions.connection = Depends(get_db_connection),
+):
+    with connection.cursor(cursor_factory=RealDictCursor) as cursor:
         cursor.execute("SELECT * FROM my_table WHERE num=%s;", (item_id,))
         return cursor.fetchone()
 
 
 @app.put("/items/{item_id}")
-def put_item(item_id: int, data_string: str = "abc'def"):
-    with CONNECTION.cursor() as cursor:
+def put_item(
+    item_id: int,
+    data_string: str = "abc'def",
+    connection: psycopg2.extensions.connection = Depends(get_db_connection),
+):
+    with connection.cursor() as cursor:
         cursor.execute(
             "INSERT INTO my_table (num, data) VALUES (%s, %s) " "RETURNING *;",
             (item_id, data_string),
         )
-        CONNECTION.commit()
+        connection.commit()
         return cursor.fetchone()
 
 
 @app.delete("/items/{item_id}")
-def delete_item(item_id: int):
-    with CONNECTION.cursor() as cursor:
+def delete_item(
+    item_id: int,
+    connection: psycopg2.extensions.connection = Depends(get_db_connection),
+):
+    with connection.cursor() as cursor:
         cursor.execute("DELETE FROM my_table WHERE num=%s RETURNING *;", (item_id,))
-        CONNECTION.commit()
+        connection.commit()
         return cursor.fetchone()
 
 
 if __name__ == "__main__":
-    try:
-        CONNECTION = psycopg2.connect(
-            dbname="postgres", user="postgres", host="my_db", port=5432
-        )
-        create_database()
-        uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
-    finally:
-        CONNECTION.close()
+    create_database()
+    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
